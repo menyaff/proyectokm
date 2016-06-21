27735b9914cfa63ec17d05e09a5e76dc1346b1ab
Pablo Almaguer <pablodanielalmaguer@gmail.com>
2016-06-21 14:09:00 -0500

    articulos.php

--- articulos.php
+++ articulos.php
@@ -22,7 +22,10 @@
         jsonFields = {
                         id: { type: "number" },
                         nombre: { type: "string" },
-                        familia: { type: "string" }
+                        area: { type: "string" },
+                        familia: { type: "string" },
+                        subfamilia: { type: "string" },
+                        existencias: { type: "string" }
                     };
         jsonColumns = [
                         templateID,
@@ -31,8 +34,20 @@
                             title: "Nombre"
                         },
                         {
+                            field: "area",
+                            title: "Área"
+                        },
+                        {
                             field: "familia",
                             title: "Familia"
+                        },
+                        {
+                            field: "subfamilia",
+                            title: "Subfamilia"
+                        },
+                        {
+                            field: "existencias",
+                            title: "Existencias"
                         },
                         templateBotones
                     ];
@@ -43,123 +58,90 @@
         $(document).ready(function(){
             $(modal).setModal("Articulos", 550);
             $(grid).setGrid();
+            $("#selLugares").rellenaSelect("<?= $pathWS ?>WS_lugares.php");
+            $("#selAreas").rellenaSelect("<?= $pathWS ?>WS_areas.php");
         });
     </script>
 </head>
 <body>
-<body>
     <div id="wrapper">
-        <?php include 'menu.php'; ?>
+        <?php menuLateral(); ?>
         <!-- Contenido -->
         <div id="page-wrapper">
             <div class="container-fluid">
             <!-- mapa ubicación -->
                 <div class="row">
                     <ol class="breadcrumb">
-                        <li>
-                            <a href="index.php"><span class="fa fa-fw fa-home"></span> Inicio</a>
-                        </li>
-                        <li class="active">
-                            <span class="fa fa-fw fa-archive"></span> Articulos
-                        </li>
+                        <li><a href="index.php"><span class="fa fa-fw fa-home"></span> Inicio</a></li>
+                        <li class="active"><span class="fa fa-fw fa-archive"></span> Articulos</li>
                     </ol>
                 </div>
             <!-- /mapa ubicación -->
-
-                <div class="row wrapper-form-sm">
-                    <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-                     <fieldset>
-                        <legend><h2>Articulos</h2></legend>
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Clave" name="iClave">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Nombre" name="iNombre">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Linea" name="iLinea">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Marca" name="iMarca">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Modelo" name="iModelo">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Descripción" name="iDescripcion">
-                        </div>
-
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Código de Barras" name="iCodigoBarras">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Sustituto" name="iSustituto">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Unidades" name="iUnidades">
-                        </div>
-                        <div class="form-group">
-                            <input type="number" class="form-control" placeholder="Existencia" name="iExistencia">
-                        </div>
-                        <fieldset>
-                            <legend><h5>Pertenece a:</h5></legend>
-                            <div class="form-group col-md-4 col-sm-12">
-                                <select class="form-control" name="sArea">
-                                    <option selected disabled>Área</option>
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                    <option value="5">5</option>
-                                </select>
-                            </div>
-                            <div class="form-group col-md-4 col-sm-12">
-                                <select class="form-control" name="sFamilia">
-                                    <option selected disabled>Familia</option>
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                    <option value="5">5</option>
-                                </select>
-                            </div>
-                            <div class="form-group col-md-4 col-sm-12">
-                                <select class="form-control" name="sSubfamilia">
-                                    <option selected disabled>Subfamilia</option>
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                    <option value="5">5</option>
-                                </select>
-                            </div>
-                        </fieldset>
-                        <div class="form-group">
-                            <input type="file">
-                        </div>
-                        
-                     </fieldset>
-                     <br><br>
-                     <fieldset>
-                         <legend><h2>Costos y Precios</h2></legend>
-                         <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Precio de Recuperación" name="iPrecioRecuperacion">
-                         </div>
-                         <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Precio de Lista" name="iPrecioLista">
-                         </div>
-                         <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Precio de Mayoreo" name="iPrecioMayoreo">
-                         </div>
-                     </fieldset>
-                     <div class="col-md-12 text-center">
-                        <button type="submit" class="btn btn-default">Enviar</button>
-                        <button type="reset" class="btn btn-default">Limpiar</button>
-                     </div>
-                   </form>
-                </div>
+                <div id="divGrid"></div>
             </div>
         </div>
+    </div>
+    <div id="divModal" class="formPopup">
+        <form>
+            <div class="form-group">
+                <input type="hidden" id="hdnId" name="id" value="0"/>
+                <input type="text" id="iClave" class="form-control form-md" placeholder="Clave" name="clave">
+            </div>
+            <div class="form-group">
+                <input type="text" id="iNombre" class="form-control form-md" placeholder="Nombre" name="nombre">
+            </div>
+            <div class="form-group">
+                <input type="text" id="iLinea" class="form-control form-md" placeholder="Linea" name="linea">
+            </div>
+            <div class="form-group">
+                <input type="text" id="iMarca" class="form-control form-md" placeholder="Marca" name="marca">
+            </div>
+            <div class="form-group">
+                <input type="text" id="iModelo" class="form-control form-md" placeholder="Modelo" name="modelo">
+            </div>
+            <div class="form-group">
+                <input type="text" id="txtDescripcion" class="form-control form-md" placeholder="Descripción" name="descripcion">
+            </div>
+            <div class="form-group">
+                <select id="selLugares" class="form-control form-md" name="lugar">
+                    <option value="" selected disabled>Lugares...</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <input type="text" id="iCodigoBarras" class="form-control form-md" placeholder="Código de Barras" name="codigoBarras">
+            </div>
+            <div class="form-group">
+                <input type="text" id="iSustituto" class="form-control form-md" placeholder="Sustituto" name="sustituto">
+            </div>
+            <div class="form-group">
+                <input type="text" id="iUnidad" class="form-control form-md" placeholder="Unidades" name="unidades">
+            </div>
+            <div class="form-group">
+                <input type="number" id="iExistencias" class="form-control form-md" placeholder="Existencias" name="existencias">
+            </div>
+            <div class="form-group">
+                <select id="selAreas" class="form-control form-md" name="area">
+                    <option value="" selected disabled>Áreas...</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <input type="file" id="fileArchivo" class="form-control form-md" name="archivo">
+            </div>
+            <div class="form-group">
+                <input type="text" id="iPrecioRecuperacion" class="form-control form-md" placeholder="Precio Recuperación" name="precioRecuperacipn">
+            </div>
+            <div class="form-group">
+                <input type="text" id="iPrecioLista" class="form-control form-md" placeholder="Precio de Lista" name="precioLista">
+            </div>
+            <div class="form-group">
+                <input type="text" id="iPrecioMayoreo" class="form-control form-md" placeholder="Precio de Mayoreo" name="precioMayoreo">
+            </div>
+            <div class="text-center">
+                <input type="submit" id="btnAceptar" class="btn btn-default" value="Aceptar" />
+                &nbsp;
+                <input type="reset" class="btn btn-default" value="Limpiar" />
+            </div>
+        </form>
     </div>
 </body>
 </html>
